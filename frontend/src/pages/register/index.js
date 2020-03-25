bdd21821429bf5caaa5a400e528628c28766a922
dayana-sog <dayanasog23@gmail.com>
2020-03-25 20:09:52 +0000

    Estilização da rota cadastro

--- frontend/src/pages/register/index.js
+++ frontend/src/pages/register/index.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 
 import './styles.css';
+import logoImg from '../../assets/logo.svg';
 
 export default function Register() {
   return (
-    <h1>Register</h1>
+    <div className="register-conteiner">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Logo"/>
+
+          <h1>Cadastro</h1>
+          <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#E02041 "/>
+            Não tenho cadastro
+          </Link>
+        </section>
+
+        <form>
+          <input placeholder="Nome da ONG" />
+          <input type="email" placeholder="E-mail" />
+          <input placeholder="Whatsapp" />
+
+          <div className="input-group">
+            <input placeholder="Cidade" />
+            <input placeholder="Uf" style={{ width: 80 }} />
+          </div>
+
+          <button className="button" type="submit">Cadastrar</button>
+        </form>
+      </div>
+    </div>
   );
 }
